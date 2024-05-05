5feb4d3cad0a663130d3d46ff99b9b932259beac
Harun23H <147631933+Harun23H@users.noreply.github.com>
2024-05-05 21:35:59 +0100

    updated css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,35 +1,34 @@
-import cab from '../../Assets/taxiHeader.png'
-import background from '../../Assets/bgvide.mp4'
+import cab from "../../Assets/taxiHeader.png";
+import background from "../../Assets/bgvide.mp4";
 
 function Header() {
   return (
     <section id="header_cont">
       <div className="header_cont">
         <div className="sideA">
-            <h3>welcome to easy drop</h3>
-            <h1>your journey your way!</h1>
-            <p>
-              Adipisicing elit. Error
-              sapiente voluptas placeat? nesciunt quibusdam!
-            </p>
+          <h3>welcome to easy drop</h3>
+          <h1>your journey your way!</h1>
+          <p>
+            Adipisicing elit. Error sapiente voluptas placeat? nesciunt
+            quibusdam!
+          </p>
           <button>book now</button>
         </div>
         <div className="sideB">
-            <img src={cab} alt="car" />
+          <img src={cab} alt="car" />
         </div>
       </div>
       <div className="headCont02">
-        <video autoPlay muted loop id="background" >
-          <source src={background} type="video/mp4"/>
+        <video autoPlay muted loop id="background">
+          <source src={background} type="video/mp4" />
         </video>
         <div className="content">
-        <h3>welcome to easy drop</h3>
+          <div className="contentInfo">
+            <h3>welcome to easy drop</h3>
             <h1>your journey your way!</h1>
-            <p>
-              Adipisicing elit. Error
-              sapiente voluptas placeat? nesciunt
-            </p>
-          <button>book now</button>
+            <p>Adipisicing elit. Error sapiente voluptas placeat? nesciunt</p>
+            <button>book now</button>
+          </div>
         </div>
       </div>
     </section>
