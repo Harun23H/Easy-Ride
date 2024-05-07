a57f42c233b5a83a46d6a9e7c9dfff5a92c87d1e
Harun23H <147631933+Harun23H@users.noreply.github.com>
2024-05-07 18:50:08 +0100

    updated css and html

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,31 +1,67 @@
-import cab from "../../Assets/taxiHeader.png";
-// import background from "../../Assets/bgvide.mp4";
-
 function Header() {
   return (
     <header id="header_cont">
       <div className="header_cont">
         <div className="sideA">
-          <h3>welcome to easy drop</h3>
-          <h1>your journey your way!</h1>
-          <p>
-            Adipisicing elit. Error sapiente voluptas placeat? nesciunt
-            quibusdam!
-          </p>
-          <button>book now</button>
+          <div className="booking">
+            <div className="headText">
+              <h1>book taxi</h1>
+            </div>
+            <form action="book" id="form">
+              <div className="formInfo">
+                <div className="starting">
+                  <label>
+                    <span>G</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="text"
+                    placeholder="starting place"
+                    id="startingP"
+                  />
+                </div>
+                <div className="starting">
+                  <label>
+                    <span>G</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="text"
+                    placeholder="starting place"
+                    id="startingP"
+                  />
+                </div>
+                <div className="starting">
+                  <label>
+                    <span>G</span>
+                  </label>
+                  <input type="datetime-local" name="Date" id="Date-and-Time" />
+                </div>
+                <div className="starting">
+                  <label>
+                    <span>G</span>
+                  </label>
+                  <select name="select" id="select">
+                    <option value="">Select Car Type</option>
+                    <option value="">Classic</option>
+                    <option value="">Econom</option>
+                    <option value="">MiniVan</option>
+                    <option value="">SUV</option>
+                    <option value="">Wagon</option>
+                    <option value="">Special Limousine</option>
+                  </select>
+                </div>
+                <div className="starting">
+                  <label>
+                    <span>G</span>
+                  </label>
+                  <input type="number" placeholder="your number" id="number" />
+                </div>
+                <button>book now!</button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="sideB">
-          <img src={cab} alt="car" />
-        </div>
-      </div>
-      <div className="headCont02">
-        {/* <video autoPlay muted loop id="background">
-          <source src={background} type="video/mp4" />
-        </video> */}
-            <h3>welcome to easy drop</h3>
-            <h1>your journey your way!</h1>
-            <p>Adipisicing elit. Error sapiente voluptas placeat? nesciunt</p>
-            <button>book now</button>
       </div>
     </header>
   );
--- src/components/section-two/SectionTwo.jsx
+++ src/components/section-two/SectionTwo.jsx
@@ -15,30 +15,151 @@
                     <h4>types</h4>
                 </div>
                 <div className="vehicleCont">
+                    <div className="showInfo">
                     <div className="card">
                         <img src={car} alt="car" />
                         <span>Classic</span>
                     </div>
+                    <div className="carDetails">
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="showInfo">
                     <div className="card">
-                        <img src={car2} alt="car" />
+                        <img src={car} alt="car" />
                         <span>Classic</span>
                     </div>
+                    <div className="carDetails">
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="showInfo">
                     <div className="card">
-                        <img src={car3} alt="car" />
+                        <img src={car} alt="car" />
                         <span>Classic</span>
                     </div>
+                    <div className="carDetails">
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="showInfo">
                     <div className="card">
-                        <img src={car4} alt="car" />
+                        <img src={car} alt="car" />
                         <span>Classic</span>
                     </div>
+                    <div className="carDetails">
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="showInfo">
                     <div className="card">
-                        <img src={car5} alt="car" />
+                        <img src={car} alt="car" />
                         <span>Classic</span>
                     </div>
+                    <div className="carDetails">
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="showInfo">
                     <div className="card">
-                        <img src={car6} alt="car" />
+                        <img src={car} alt="car" />
                         <span>Classic</span>
                     </div>
+                    <div className="carDetails">
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                        <div className="Fline">
+                            <p>Best Rate</p>
+                            <span>$2</span>
+                        </div>
+                    </div>
+                    </div>
+
                 </div>
             </div>
 
