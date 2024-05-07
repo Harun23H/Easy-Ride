55604b65580f4f29fd26a2e62b9cb09ff752bc8c
Harun23H <147631933+Harun23H@users.noreply.github.com>
2024-05-07 12:19:29 +0100

    new section added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import './components/nav/nav.css'
-import Nav from './components/nav/Nav'
-import Header from './components/header/Header'
-import './components/header/header.css'
-import { SectionTwo } from './components/section-two/SectionTwo';
-import './components/section-two/sectionTwo.css'
+import "./App.css";
+import "./components/nav/nav.css";
+import Nav from "./components/nav/Nav";
+import Header from "./components/header/Header";
+import "./components/header/header.css";
+import { SectionTwo } from "./components/section-two/SectionTwo";
+import "./components/section-two/sectionTwo.css";
+import GetApp from "./components/getApp/GetApp";
+import "./components/getApp/getApp.css"
 function App() {
   return (
     <div className="App">
-      < Nav />
-      < Header />
-      <SectionTwo/>
+      <Nav />
+      <Header />
+      <SectionTwo />
+      <GetApp />
     </div>
   );
 }
-
 
 export default App;
---
+++ src/components/getApp/GetApp.jsx
@@ -0,0 +1,38 @@
+import mobile from '../../Assets/mobile.png'
+const GetApp = () => {
+  return (
+    <section id="getApp">
+      <div className="getApp">
+        <div className="sect01">
+          <div className="div01">
+            <small>book taxi from smartphone</small>
+            <h1>get your app</h1>
+          </div>
+          <p>
+            our app is easy to use and very user friendly. just choose your
+            start and end location, select the car you like and in just a few minutes we'll be at your doorstep.
+          </p>
+          <div className="div02">
+            <div className="line01">
+                <span></span>
+                <p>easy taxi booking</p>
+            </div>
+            <div className="line01">
+                <span></span>
+                <p>check the rate in real time</p>
+            </div>
+            <div className="line01">
+                <span></span>
+                <p>calculate the trip mileage</p>
+            </div>
+          </div>
+        </div>
+        <div className="sect02">
+            <img src={mobile} alt="mobile" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GetApp;
